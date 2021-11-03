8207c0bd233a3c345a68d5fa0cfd00d72793ed17
Daniel Roth <daroth@microsoft.com>
2021-11-03 09:02:34 -0700

    Switch rendering mode

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
